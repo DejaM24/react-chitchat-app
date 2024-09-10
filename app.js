ca3e6332e8525195fd18c0b6df6d3050acdd9e16
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-09-10 15:11:10 -0400

    backend updates

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
 import indexRouter from "./routes/index.js";
 import testRouter from"./routes/test.js";
+import mongoose from "mongoose";
 
 
+//runs express server
 const app = express();
-const port = 5500;
+app.use(express.json());
+app.use(cors());
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`React-chitchat-app is now running on port ${process.env.SERVER_PORT}.`)
+});
 
 //set routes from handlers/endpoints
 app.use("/", indexRouter);
 app.use("/test", testRouter);
 
+//mongoose connection
+mongoose.connect(process.env.ATLAS_CONNECTION);
+const db = mongoose.connection;
 
-//starts server
-app.listen(port, () => {
-    console.log(`React-chitchat-app is now running on port ${port}.`)
+db.once("open", () => {
+    console.log("Database successfully connected.");
 });
+
+db.on("error", console.error.bind(console, "Connection error:"))
+

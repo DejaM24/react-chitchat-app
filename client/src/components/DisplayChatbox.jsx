12b1a6df1a4ad714fe359afbf18df6f20f8b4e48
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-10-21 02:55:33 -0400

    functional chatbox button

--- client/src/components/DisplayChatbox.jsx
+++ client/src/components/DisplayChatbox.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import DisplayDate from './DisplayDate.jsx';
-import DisplayMessages from "./DisplayMessages.jsx";
+import { format } from 'date-fns';
 
 export default function DisplayChatbox() {
     const [posts, setPosts] = useState([]);
     let { room } = useParams();
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, 'MMMM dd, yyyy');
 
     useEffect(() => {
         fetch(`http://localhost:3000/message/${room}`)
@@ -20,10 +21,9 @@ export default function DisplayChatbox() {
             <div>
                 {posts.map((list, index) => (
                     <h2 key={index} className="border-4 border-solid rounded-full border-white p-5 mb-9 font-bold">
-                        {/* <div className="text-2xl" >User:</div> */}
-                        <div className="text-4xl mb-2">User: {list.user} <br></br></div>
-                        <div className="pb-5 font-teal">Message: {list.post}<br></br></div>
-                        <div>{list.date}</div><br></br>
+                        <div className="text-3xl mb-2">{list.user} <br></br></div>
+                        <div className="text-2xl pb-5 font-teal">Message: {list.post}<br></br></div>
+                        <div className="text-md">Posted: {formattedDate}</div><br></br>
                     </h2>
                 ))}
             </div>
